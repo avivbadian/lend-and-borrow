3a08b2a12257391c95719688ceba3716612429eb
yaniv <yanivkrim@gmail.com>
2019-08-17 22:55:19 +0300

    Updated DB script to have more tables and values

--- loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_schemas.sql
+++ loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_schemas.sql
@@ -1,16 +1,104 @@
-DROP TABLE users;
-DROP TABLE items;
+﻿DROP TABLE IF EXISTS borrows;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE users(
-    uid char(30) not null,
-	username char(30) not null,
-	status char(30) not null,
-	PRIMARY KEY (uid)
+CREATE TABLE "category"
+(
+ "id"   numeric NOT NULL,
+ "name" char(50) NOT NULL
+
 );
 
-CREATE TABLE items(
-	iid numeric(5,0),
-	title varchar(50) not null,
-	description char(30),
-	PRIMARY KEY(iid)
+CREATE UNIQUE INDEX "PK_category" ON "category"
+(
+ "id"
 );
+
+CREATE TABLE "users"
+(
+ "uid"      numeric NOT NULL,
+ "username" char(50) NOT NULL,
+ "status"   char(50) NOT NULL
+
+);
+
+CREATE UNIQUE INDEX "PK_users" ON "users"
+(
+ "uid"
+);
+
+
+CREATE TABLE "items"
+(
+ "iid"         numeric(18,0) NOT NULL,
+ "ownerId"     numeric NOT NULL,
+ "name"        char(50) NOT NULL,
+ "description" char(50) NOT NULL,
+ "categoryId"  numeric NOT NULL,
+ CONSTRAINT "FK_11" FOREIGN KEY ( "ownerId" ) REFERENCES "users" ( "uid" ),
+ CONSTRAINT "FK_37" FOREIGN KEY ( "categoryId" ) REFERENCES "category" ( "id" )
+);
+
+CREATE UNIQUE INDEX "PK_items" ON "items"
+(
+ "iid"
+);
+
+CREATE INDEX "fkIdx_11" ON "items"
+(
+ "ownerId"
+);
+
+CREATE INDEX "fkIdx_37" ON "items"
+(
+ "categoryId"
+);
+
+CREATE TABLE "borrows"
+(
+ "borrower"   numeric NOT NULL,
+ "itemId"     numeric(18,0) NOT NULL,
+ "borrowDate" date NOT NULL,
+ "returnDate" date NOT NULL,
+ "status"     numeric NOT NULL,
+ CONSTRAINT "FK_56" FOREIGN KEY ( "borrower" ) REFERENCES "users" ( "uid" ),
+ CONSTRAINT "FK_59" FOREIGN KEY ( "itemId" ) REFERENCES "items" ( "iid" )
+);
+
+
+CREATE INDEX "fkIdx_56" ON "borrows"
+(
+ "borrower"
+);
+
+CREATE INDEX "fkIdx_59" ON "borrows"
+(
+ "itemId"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_testdata.sql
+++ loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_testdata.sql
@@ -1,10 +1,15 @@
-DELETE FROM users;
+﻿DELETE FROM users;
 DELETE FROM items;
 
-INSERT INTO users VALUES 
-('avivbadian', 'Aa123456'),
-('yanivkrim', 'Aa123456');
 
+# uid, username,satus
+INSERT INTO users VALUES 
+(1, 'avivbadian','Gever Gever'),
+(2, 'yanivkrim', 'Totah');
+
+#iid, ownerId (fk uid),name,description
 INSERT INTO items VALUES 
-(12345, 'Tent', '6 people tent'),
-(123, 'Book', 'Harry Potter 3rd book');
+(1, ,1,'Tent', '6 people tent'),
+(2, ,1,'Book', 'Harry Potter 3rd book')
+(3, ,2,'Book', 'Advanced java')
+(4, ,2,'Chair', 'Keter chair, good for throwing');
