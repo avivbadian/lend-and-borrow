1c7a97aba9b248503ffa43fe4828d65e82b96f8c
yaniv <yanivkrim@gmail.com>
2019-08-26 22:10:47 +0300

    Added Branches controller

---
+++ loan-rest-server/src/main/java/LoanStuff/Controllers/BranchesController.java
@@ -0,0 +1,58 @@
+package LoanStuff.Controllers;
+
+import LoanStuff.DB.DataStore;
+import LoanStuff.ViewModels.Branch;
+import LoanStuff.ViewModels.Item;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+@RestController
+public class BranchesController {
+    private DataStore db;
+
+    public BranchesController() throws SQLException, ClassNotFoundException {
+        db = new DataStore();
+    }
+
+    @GetMapping("/branches")
+    public ArrayList<Branch> getAllBranches() {
+        ArrayList<Branch> branches = new ArrayList<>();
+        ResultSet rs;
+        try {
+            rs = db.execQuery("SELECT * FROM branches");
+            while (rs.next()) {
+                //-- id, title, address
+                Branch branch = new Branch();
+                branch.Id = rs.getInt(1);
+                branch.Title = rs.getString(2).trim();
+                branch.Address = rs.getString(3).trim();
+
+                branches.add(branch);
+            }
+        } catch (SQLException e) {
+            // TODO: log or something
+            return null;
+        }
+
+        return branches;
+    }
+
+    @PostMapping("/branches")
+    public ResponseEntity postAddBranch(@RequestBody Branch newBranch) {
+        try {
+            db.execUpdate(String.format("INSERT INTO branches VALUES (DEFAULT, '%s', '%s')", newBranch.Title, newBranch.Address));
+
+            return new ResponseEntity(HttpStatus.CREATED);
+        } catch (SQLException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
