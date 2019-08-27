45e006f5cf9979cba531ae0b703b562bfe2e13dc
yaniv <yanivkrim@gmail.com>
2019-08-27 20:51:15 +0300

    Added get item's availabilities and delete item by id

--- loan-rest-server/src/main/java/LoanStuff/Controllers/ItemsController.java
+++ loan-rest-server/src/main/java/LoanStuff/Controllers/ItemsController.java
@@ -1,6 +1,7 @@
 package LoanStuff.Controllers;
 
 import LoanStuff.DB.DataStore;
+import LoanStuff.ViewModels.Availability;
 import LoanStuff.ViewModels.Item;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ItemsController {
@@ -72,6 +74,42 @@ public class ItemsController {
             db.execUpdate(String.format("INSERT INTO items VALUES\n(DEFAULT,'%s','%s','%s')", newItem.Title, newItem.Category, newItem.Description));
 
             return new ResponseEntity(HttpStatus.CREATED);
+        } catch (SQLException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/items/{id}/availability")
+    public ArrayList<Availability> getAllItemAvailabilities(@PathVariable int id)
+    {
+        ArrayList<Availability> availability = new ArrayList<>();
+        ResultSet rs;
+        try {
+            rs = db.execQuery(String.format("SELECT * FROM availabilities WHERE item_id='%d'", id));
+            while (rs.next()) {
+                Availability newAval = new Availability();
+                newAval.Id = rs.getInt(1);
+                newAval.Item_id = id;
+                newAval.Branch = rs.getInt(3);
+                newAval.Start_date = rs.getDate(4);
+                newAval.End_date = rs.getDate(5);
+
+                availability.add(newAval);
+            }
+        } catch (SQLException e) {
+            // TODO: log or something
+            return null;
+        }
+
+        return availability;
+    }
+
+
+    @DeleteMapping("/items/{id}")
+    public ResponseEntity deleteBranch(@PathVariable int id) {
+        try {
+            db.execUpdate(String.format("DELETE FROM items WHERE id='%s'", id));
+
+            return new ResponseEntity(HttpStatus.OK);
         } catch (SQLException e) {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
