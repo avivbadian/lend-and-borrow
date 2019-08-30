ef3145874f26f582ac90c3cc82bab9accc80481b
Aviv Badian <avivbadian@gmail.com>
2019-08-30 20:00:28 +0300

    fix test data

--- loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_testdata.sql
+++ loan-rest-server/src/main/java/LoanStuff/DB/Scripts/db_testdata.sql
@@ -10,11 +10,11 @@ INSERT INTO admins VALUES
 ('avivbadian', 'KobiZNoti');
 	
 --- id, title, category, description
-INSERT INTO items VALUES
-(DEFAULT,'Keter Chair', 'Camping Equipment', 'Original Keter chair'),
-(DEFAULT, 'Harry Potter: The Philosophers stone', 'Books', 'Harry Potter book first book'),
-(DEFAULT, 'Harry Potter: The Chamber of Secrets', 'Books', 'Harry Potter book second book'),
-(DEFAULT, 'Harry Potter: The Prisoner of Azkaban', 'Books', 'Harry Potter book third book');
+INSERT INTO items (title, category, description) VALUES
+('Keter Chair', 'Camping Equipment', 'Original Keter chair'),
+('Harry Potter: The Philosophers stone', 'Books', 'Harry Potter book first book'),
+('Harry Potter: The Chamber of Secrets', 'Books', 'Harry Potter book second book'),
+('Harry Potter: The Prisoner of Azkaban', 'Books', 'Harry Potter book third book');
 
 
 -- title, address
@@ -25,29 +25,25 @@ INSERT INTO branches VALUES
 ('Disney', 'Paris');
 
 -- id, item_id, start_date, end_date
-INSERT INTO availabilities VALUES
-(DEFAULT,1, '01/10/2019', '08/10/2019'),
-(DEFAULT,1, '10/10/2019', '17/10/2019'),
-(DEFAULT,1, '01/11/2019', '08/11/2019'),
-(DEFAULT,1, '10/11/2019', '17/11/2019'),
-(DEFAULT,2, '01/10/2019', '08/10/2019'),
-(DEFAULT,2, '10/10/2019', '17/10/2019'),
-(DEFAULT,2, '01/11/2019', '08/11/2019'),
-(DEFAULT,2, '10/11/2019', '17/11/2019'),
-(DEFAULT,3, '01/10/2019', '08/10/2019'),
-(DEFAULT,3, '10/10/2019', '17/10/2019'),
-(DEFAULT,3, '01/11/2019', '08/11/2019'),
-(DEFAULT,3, '10/11/2019', '17/11/2019'),
-(DEFAULT,4, '01/10/2019', '08/10/2019'),
-(DEFAULT,4, '10/10/2019', '17/10/2019'),
-(DEFAULT,4, '01/11/2019', '08/11/2019'),
-(DEFAULT,4, '10/11/2019', '17/11/2019');
+INSERT INTO availabilities (item_id, start_date, end_date) VALUES
+((SELECT id from items WHERE title = 'Keter Chair') , '01/10/2019', '08/10/2019'),
+((SELECT id from items WHERE title = 'Keter Chair'), '10/10/2019', '17/10/2019'),
+((SELECT id from items WHERE title = 'Keter Chair'), '01/11/2019', '08/11/2019'),
+((SELECT id from items WHERE title = 'Keter Chair'), '10/11/2019', '17/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Philosophers stone'), '01/10/2019', '08/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Philosophers stone'), '10/10/2019', '17/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Philosophers stone'), '01/11/2019', '08/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Philosophers stone'), '10/11/2019', '17/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Chamber of Secrets'), '01/10/2019', '08/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Chamber of Secrets'), '10/10/2019', '17/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Chamber of Secrets'), '01/11/2019', '08/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Chamber of Secrets'), '10/11/2019', '17/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Prisoner of Azkaban'), '01/10/2019', '08/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Prisoner of Azkaban'), '10/10/2019', '17/10/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Prisoner of Azkaban'), '01/11/2019', '08/11/2019'),
+((SELECT id from items WHERE title = 'Harry Potter: The Prisoner of Azkaban'), '10/11/2019', '17/11/2019');
 
 --- id, availability, phone, email,first name, last name
-INSERT INTO Borrows VALUES
-(DEFAULT, 1, '054-555666', 'theif@gmail.com', 'Gani', 'Nav'),
-(DEFAULT, 2, '053-123456', 'yossi@gmail.com', 'Yossi', 'Luchim'),
-(DEFAULT, 5, '052-777888', 'racheli@gmail.com', 'Racheli', 'krakdfsdfki'),
-(DEFAULT, 7, '050-409040', 'ronit@gmail.com', 'Ronit', 'Gal-Or'),
-(DEFAULT, 9, '04-8470475', 'eitan@gmail.com', 'Eitan', 'Barak'),
-(DEFAULT, 10, '058-401474', 'ahmed@gmail.com', 'Ahmed', 'Abbas');
+INSERT INTO Borrows (availability, branch, phone, email, first_name, last_name) VALUES
+((SELECT max(id) from availabilities), 'Haifa-Main', '054-555666', 'theif@gmail.com', 'Gani', 'Nav'),
+((SELECT min(id) from availabilities), 'Haifa-Main', '053-123456', 'yossi@gmail.com', 'Yossi', 'Luchim')
