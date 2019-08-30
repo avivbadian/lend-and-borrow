d379d50b0bd387e992f6118f97aa560493c5d53b
Aviv Badian <avivbadian@gmail.com>
2019-08-30 20:40:07 +0300

    fix models deserialize

--- loan-rest-server/src/main/java/LoanStuff/ViewModels/Borrow.java
+++ loan-rest-server/src/main/java/LoanStuff/ViewModels/Borrow.java
@@ -1,27 +1,20 @@
 package LoanStuff.ViewModels;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.Date;
+import java.io.Serializable;
 
 //--- id, availability, borrow_date, return_date, phone, email,first name, last name
-public class Borrow {
-    @JsonProperty("id")
+public class Borrow implements Serializable {
+
     public int Id;
 
-    @JsonProperty("availability")
     public int Availability;
 
-    @JsonProperty("phone")
     public String Phone;
 
-    @JsonProperty("email")
     public String Email;
 
-    @JsonProperty("first_name")
     public String First_name;
 
-    @JsonProperty("last_name")
     public String Last_name;
 
 }
